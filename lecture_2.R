509a742695df4c488f86da57fc9e13badc869883
Andy Hasselwander <ahasselwander@employeenavigator.com>
2018-01-29 17:35:27 -0500

    more lecture notes

--- lecture_2.R
+++ lecture_2.R
@@ -14,3 +14,28 @@ predictions <- predict(modelFit, newdata = testing)
 predictions
 
 confusionMatrix(predictions, testing$type)
+
+folds <- createFolds(y=spam$type, k=10, 
+                     list = TRUE, returnTrain = TRUE)
+sapply(folds, length)
+folds[[1]][1:10]
+
+#return only the test set samples:
+
+folds <- createFolds(y=spam$type, k=10,
+                     list=TRUE,returnTrain = FALSE)
+sapply(folds, length)
+
+#resampling with replacement. You might get the observation repeated multiple times.
+
+folds <- createResample(y=spam$type, times = 10, list=TRUE)
+sapply(folds, length)
+folds[[1]][1:10]
+
+# You can set lots of parameters when training...
+
+modelFit <- train(type ~., data=training, method = "glm")
+args(train.default) #this doesn't work
+
+
+
