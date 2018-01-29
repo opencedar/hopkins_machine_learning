2483a67a6a9094a30ab8c7e76624ea165621ab59
Andy Hasselwander <ahasselwander@employeenavigator.com>
2018-01-29 17:08:41 -0500

    initial commit

---
+++ lecture_2.R
@@ -0,0 +1,16 @@
+library(caret); library(kernlab); data(spam)
+inTrain <- createDataPartition(y=spam$type,
+                               p=0.75, list=FALSE)
+training <- spam[inTrain,]
+testing <- spam[-inTrain,]
+dim(training)
+
+set.seed(32343)
+modelFit <- train(type ~., data=training, method="glm")
+modelFit
+modelFit$finalModel
+
+predictions <- predict(modelFit, newdata = testing)
+predictions
+
+confusionMatrix(predictions, testing$type)
