8dea03e238a40b9c93d51ce7f81e9993f33e6d9d
Andy Hasselwander <ahasselwander@gmail.com>
2018-02-16 13:10:02 -0500

    Quiz 4

--- lecture_4.R
+++ lecture_4.R
@@ -75,3 +75,80 @@ accuracy(predCombMod, predDFTest$diagnosis)
 
 #Stacked Accuracy: 0.80 is better than random forests and lda and the same as boosting.`
 
+#3
+#Load the concrete data with the commands:
+set.seed(3523)
+library(AppliedPredictiveModeling)
+library(caret)
+data(concrete)
+inTrain = createDataPartition(concrete$CompressiveStrength, p = 3/4)[[1]]
+training = concrete[ inTrain,]
+testing = concrete[-inTrain,]
+
+
+#Set the seed to 233 and fit a lasso model to predict Compressive Strength. 
+#Which variable is the last coefficient to be set to zero as the penalty 
+#increases? (Hint: it may be useful to look up ?plot.enet).
+
+set.seed(233)
+lasso.Fit <- train(CompressiveStrength ~., data=training, method="lasso")
+require(elasticnet)
+
+plot(lasso.Fit$finalModel, "penalty")
+#Cement
+
+#4
+#Load the data on the number of visitors to the instructors blog from here:
+  
+#https://d396qusza40orc.cloudfront.net/predmachlearn/gaData.csv
+
+library(RCurl)
+dat <- read.csv(url("https://d396qusza40orc.cloudfront.net/predmachlearn/gaData.csv"))
+
+#Using the commands:
+
+library(lubridate) # For year() function below
+
+training = dat[year(dat$date) < 2012,]
+testing = dat[(year(dat$date)) > 2011,]
+tstrain = ts(training$visitsTumblr)
+tsTest <- ts(testing$visitsTumblr)
+
+#Fit a model using the bats() function in the forecast package to 
+#the training time series. Then forecast this model for the remaining time 
+#points. For how many of the testing points is the true value within the 95% 
+#prediction interval bounds?
+
+library(forecast)
+tsModel <- bats(tstrain)
+forecast <- predict(tsModel, nrow(testing), level=c(80,95))
+upperBounds <- forecast$upper
+insideBounds <- testing$visitsTumblr < upperBounds[,2]
+sum(as.numeric(insideBounds)) / nrow(testing)
+
+#226, 96%
+
+#5
+
+#Load the concrete data with the commands:
+
+set.seed(3523)
+library(AppliedPredictiveModeling)
+data(concrete)
+inTrain = createDataPartition(concrete$CompressiveStrength, p = 3/4)[[1]]
+training = concrete[ inTrain,]
+testing = concrete[-inTrain,]
+
+#Set the seed to 325 and fit a support vector machine using the e1071 package 
+#to predict Compressive Strength using the default settings. 
+#Predict on the testing set. What is the RMSE?
+
+set.seed(325)
+library(e1071)
+model <- svm(CompressiveStrength ~ ., data = training)
+prediction <- predict(model, testing)
+E <- prediction-testing$CompressiveStrength
+SE <- sum(E^2)/length(E)
+RMSE <- (SE)^0.5
+
+#RMSE = 6.715009
